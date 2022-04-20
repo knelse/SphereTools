3115be8ffd923ddf7617b7bbce7f9cd3b2b17669
Andrey Klyuev <akluyev@iponweb.net>
2022-04-20 22:04:38 +0100

    Tools
    Client packet decoder (manual for now), hex to binary string converter, string comparison highlighter

---
+++ HexToBinaryString/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Text;
+
+while (true)
+{
+    try
+    {
+        var str = Console.ReadLine().Replace(" ", "");
+        Console.WriteLine(ByteArrayToBinaryString(Convert.FromHexString(str)));
+        Console.WriteLine();
+    }
+    catch
+    {
+        Console.WriteLine("Bad input, try again");
+    }
+}
+
+static string ByteArrayToBinaryString(byte[] ba, bool noPadding = false)
+{
+    var hex = new StringBuilder(ba.Length * 2);
+    
+    foreach (var val in ba)
+    {
+        var str = Convert.ToString(val, 2);
+        if (!noPadding) str = str.PadLeft(8, '0');
+        hex.Append(str);
+        //hex.Append(' ');
+    }
+    
+    return hex.ToString();
+}
