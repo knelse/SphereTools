b898dd4f6d44fb568f25606b8156794b5e2e3e7f
Andrey Klyuev <klandrew@gmail.com>
2023-08-10 18:48:40 +0100

    Landscape import starts to work

--- sphModelImport/ModelImport.cs
+++ sphModelImport/ModelImport.cs
@@ -10,109 +10,117 @@ public partial class ModelImport : MeshInstance3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// var path = @"c:\Games\Sfera\models\";
-		// var files = Directory.EnumerateFiles(path, "*.mdl");
-		//
-		// foreach (var file in files)
-		// {
-		// 	var modelInfo = SphModelInfo.GetModelInfo(file);
-		//
-		// 	if (modelInfo is null)
-		// 	{
-		// 		Console.WriteLine($"Skipping {file}");
-		//
-		// 		continue;
-		// 	}
-		//
-		// 	var arrayMesh = new ArrayMesh();
-		//
-		// 	foreach (var surface in modelInfo.Surfaces)
-		// 	{
-		// 		var surfaceArray = new Godot.Collections.Array();
-		// 		surfaceArray.Resize((int) Mesh.ArrayType.Max);
-		// 		var vertices = new List<Vector3>();
-		// 		var uvs = new List<Vector2>();
-		// 		var normals = new List<Vector3>();
-		// 		var indices = new List<int>();
-		//
-		// 		for (var i = surface.FirstVertexIndex; i < surface.FirstVertexIndex + surface.VertexCount; i++)
-		// 		{
-		// 			var vertex = modelInfo.Vertices[i];
-		// 			vertices.Add(new Vector3(vertex.X, -vertex.Y, vertex.Z));
-		// 			normals.Add(new Vector3(vertex.NormalX, -vertex.NormalY, vertex.NormalZ));
-		// 			uvs.Add(new Vector2(vertex.TextureU, vertex.TextureV));
-		// 		}
-		//
-		// 		for (var i = surface.FirstTriangleIndex; i < surface.FirstTriangleIndex + surface.TriangleCount; i++)
-		// 		{
-		// 			var triangle = modelInfo.Triangles[i];
-		// 			indices.Add(triangle.Vertex1);
-		// 			indices.Add(triangle.Vertex2);
-		// 			indices.Add(triangle.Vertex3);
-		// 		}
-		//
-		// 		surfaceArray[(int) Mesh.ArrayType.Vertex] = vertices.ToArray();
-		// 		surfaceArray[(int) Mesh.ArrayType.TexUv] = uvs.ToArray();
-		// 		surfaceArray[(int) Mesh.ArrayType.Normal] = normals.ToArray();
-		// 		surfaceArray[(int) Mesh.ArrayType.Index] = indices.ToArray();
-		//
-		// 		arrayMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles, surfaceArray);
-		// 	}
-		//
-		// 	ResourceSaver.Save(arrayMesh, $"res://{Path.GetFileNameWithoutExtension(file)}.tres", ResourceSaver.SaverFlags.Compress);
-		// }
+		var path = @"c:\Games\Sfera\models\";
+		var modelFiles = Directory.EnumerateFiles(path, "*.mdl");
+		
+		foreach (var file in modelFiles)
+		{
+			var modelInfo = SphModelInfo.GetModelInfo(file);
+		
+			if (modelInfo is null)
+			{
+				Console.WriteLine($"Skipping {file}");
+		
+				continue;
+			}
+		
+			var arrayMesh = new ArrayMesh();
+		
+			foreach (var surface in modelInfo.Surfaces)
+			{
+				var surfaceArray = new Godot.Collections.Array();
+				surfaceArray.Resize((int) Mesh.ArrayType.Max);
+				var vertices = new List<Vector3>();
+				var uvs = new List<Vector2>();
+				var normals = new List<Vector3>();
+				var indices = new List<int>();
+		
+				for (var i = surface.FirstVertexIndex; i < surface.FirstVertexIndex + surface.VertexCount; i++)
+				{
+					var vertex = modelInfo.Vertices[i];
+					vertices.Add(new Vector3(vertex.X, -vertex.Y, vertex.Z));
+					normals.Add(new Vector3(vertex.NormalX, -vertex.NormalY, vertex.NormalZ));
+					uvs.Add(new Vector2(vertex.TextureU, vertex.TextureV));
+				}
+		
+				for (var i = surface.FirstTriangleIndex; i < surface.FirstTriangleIndex + surface.TriangleCount; i++)
+				{
+					var triangle = modelInfo.Triangles[i];
+					indices.Add(triangle.Vertex1);
+					indices.Add(triangle.Vertex2);
+					indices.Add(triangle.Vertex3);
+				}
+		
+				surfaceArray[(int) Mesh.ArrayType.Vertex] = vertices.ToArray();
+				surfaceArray[(int) Mesh.ArrayType.TexUV] = uvs.ToArray();
+				surfaceArray[(int) Mesh.ArrayType.Normal] = normals.ToArray();
+				surfaceArray[(int) Mesh.ArrayType.Index] = indices.ToArray();
+		
+				arrayMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles, surfaceArray);
+			}
+		
+			ResourceSaver.Save(arrayMesh, $"res://models/{Path.GetFileNameWithoutExtension(file)}.tres", ResourceSaver.SaverFlags.Compress);
+		}
 
-		var landscapePath = @"c:\Games\Sfera\landscape\";
+		var landscapePath = @"c:\Games\Sfera_std\landscape\";
 		var files = Directory.EnumerateFiles(landscapePath, "*.lnd");
-
-		using var file = File.OpenRead(files.First());
-		using var reader = new BinaryReader(file);
-		var magicBytes = reader.ReadBytes(4);
-		var totalVertexCount = reader.ReadUInt16();
-
-		file.Seek(0x1b20, SeekOrigin.Begin);
-
-		var vertices = new List<Vector3>();
-		var normals = new List<Vector3>();
-		var uvs = new List<Vector2>();
-
-		while (file.Position < file.Length - 12)
-		// for (var i = 0; i < totalVertexCount; i++)
+		foreach (var filePath in files)
 		{
-			// var vertex = reader.ReadStruct<Vertex>();
-			// Console.WriteLine($"{vertex.X} {vertex.Y} {vertex.Z} {vertex.NormalX} {vertex.NormalY} {vertex.NormalZ} {vertex.TextureU} {vertex.TextureV}");
-			var x = reader.ReadSingle();
-			var y = reader.ReadSingle();
-			var z = reader.ReadSingle();
-			vertices.Add(new Vector3(x, -y, z));
-			// var xN = reader.ReadSingle();
-			// var yN = reader.ReadSingle();
-			// var zN = reader.ReadSingle();
-			normals.Add(new Vector3(x, -y, z));
-			uvs.Add(new Vector2(0, 0));
+
+			using var file = File.OpenRead(filePath);
+			using var reader = new BinaryReader(file);
+			var magicBytes = reader.ReadBytes(4);
+			var totalVertexCount = reader.ReadUInt16();
+
+			file.Seek(0x68c0, SeekOrigin.Begin);
+
+			var vertices = new List<Vector3>();
+			var normals = new List<Vector3>();
+			var uvs = new List<Vector2>();
+
+			for (var i = 0; i < totalVertexCount; i++)
+			{
+				var x = reader.ReadSingle();
+				var y = reader.ReadSingle();
+				var z = reader.ReadSingle();
+				var xN = reader.ReadSingle();
+				var yN = reader.ReadSingle();
+				var zN = reader.ReadSingle();
+				var u = reader.ReadSingle();
+				var v = reader.ReadSingle();
+				var unknown = reader.ReadBytes(8);
+				vertices.Add(new Vector3(x, -y, z));
+				normals.Add(new Vector3(xN, -yN, zN));
+				uvs.Add(new Vector2(u, v));
+			}
+
+			var arrayMesh = new ArrayMesh();
+
+			var surfaceArray = new Godot.Collections.Array();
+			surfaceArray.Resize((int)Mesh.ArrayType.Max);
+			var indices = new List<int>();
+
+			while (file.Position < file.Length)
+			{
+				var v1 = reader.ReadUInt16();
+				var v2 = reader.ReadUInt16();
+				var v3 = reader.ReadUInt16();
+				reader.ReadBytes(22);
+				indices.Add(v1);
+				indices.Add(v2);
+				indices.Add(v3);
+			}
+
+			surfaceArray[(int)Mesh.ArrayType.Vertex] = vertices.ToArray();
+			surfaceArray[(int)Mesh.ArrayType.TexUV] = uvs.ToArray();
+			surfaceArray[(int)Mesh.ArrayType.Normal] = normals.ToArray();
+			surfaceArray[(int)Mesh.ArrayType.Index] = indices.ToArray();
+
+			arrayMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles, surfaceArray);
+
+			ResourceSaver.Save(arrayMesh, $"res://landscape/{Path.GetFileNameWithoutExtension(filePath)}.tres",
+				ResourceSaver.SaverFlags.Compress);
 		}
-
-		var arrayMesh = new ArrayMesh();
-
-		var surfaceArray = new Godot.Collections.Array();
-		surfaceArray.Resize((int) Mesh.ArrayType.Max);
-		var indices = new List<int>();
-
-		for (var i = 0; i < vertices.Count; i++)
-		{
-			indices.Add(i);
-			// indices.Add(i + 1);
-			// indices.Add(i + 2);
-		}
-
-		surfaceArray[(int) Mesh.ArrayType.Vertex] = vertices.ToArray();
-		surfaceArray[(int) Mesh.ArrayType.TexUv] = uvs.ToArray();
-		surfaceArray[(int) Mesh.ArrayType.Normal] = normals.ToArray();
-		surfaceArray[(int) Mesh.ArrayType.Index] = indices.ToArray();
-
-		arrayMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Points, surfaceArray);
-
-		ResourceSaver.Save(arrayMesh, $"res://testlnd.tres", ResourceSaver.SaverFlags.Compress);
 
 	}
 
