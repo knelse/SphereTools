10c23a734e84544d0427743badb6cd078e58ff32
Andrey Klyuev <klandrew@gmail.com>
2023-11-06 21:51:02 +0000

    Better sph time

--- PacketLogViewer/MainWindow.xaml.cs
+++ PacketLogViewer/MainWindow.xaml.cs
@@ -96,8 +96,8 @@ namespace PacketLogViewer
 
         public void UpdateGameTime()
         {
-            var time = TimeHelper.GetCurrentSphereDateTime();
-            GameTime.Text = time.ToString("dd/MM/yyyy hh:mm");
+            var time = TimeHelper.GetCurrentSphereDateTime().AddYears(7800);
+            GameTime.Text = time.ToString("dd/MM/yyyy HH:mm");
             GameTimeBits.Text = ByteArrayToBinaryString(TimeHelper.EncodeCurrentSphereDateTime(), false, true);
         }
 
@@ -496,15 +496,12 @@ namespace PacketLogViewer
     }
     public static class TimeHelper
     {
-        public const int UnixTimeOrigin = 1691525655;
-        public static readonly DateTime SphereTimeOrigin = new (8100, 8, 3, 2, 49, 15);
+        public static readonly DateTime RealtimeOrigin = new (1998, 8, 21, 10, 00, 6);
 
         public static DateTime GetCurrentSphereDateTime()
         {
-            var now = new DateTimeOffset(DateTime.UtcNow);
-            var fromUnixTimeOrigin = now.ToUnixTimeSeconds() - UnixTimeOrigin;
-            var sphereTimeOffset = fromUnixTimeOrigin * 12;
-            var sphereDateTime = SphereTimeOrigin.AddSeconds(sphereTimeOffset);
+            var sphereTimeOffset = (DateTime.Now - RealtimeOrigin).TotalSeconds * 12;
+            var sphereDateTime = new DateTime().AddSeconds(sphereTimeOffset);
 
             return sphereDateTime;
         }
@@ -513,10 +510,10 @@ namespace PacketLogViewer
         public static byte[] EncodeCurrentSphereDateTime()
         {
             var currentSphereTime = GetCurrentSphereDateTime();
-            // ingame time shows hours and minutes, we'll ignore seconds
+            var seconds = currentSphereTime.Second / 12;
             var minutes_last4 = (byte)((currentSphereTime.Minute & 0b1111) << 4);
-            // 1-4 minutes 5-8 idk 
-            var firstDateByte = (byte)(minutes_last4 + 0b1000);
+            // 1-4 minutes 5-8 seconds 
+            var firstDateByte = (byte)(minutes_last4 + (seconds & 0b1111));
             var minutes_first2 = (byte)((currentSphereTime.Minute & 0b110000) >> 4);
             var hours = (byte)(currentSphereTime.Hour << 2);
             var days_last1 = (byte)((currentSphereTime.Day % 2) << 7);
@@ -527,14 +524,12 @@ namespace PacketLogViewer
             // 1-4 months 5-8 days
             var thirdDateByte = (byte)(month + days_first4);
             var years_last8 = (byte)(currentSphereTime.Year & 0b11111111);
-            var years_first1 = (byte)((currentSphereTime.Year & 0b100000000) >> 8);
-            // quite likely it has more digits but 20+ irl years should be enough
-            var fourthDateByte = (byte)(0b00110100 + years_first1);
+            var years_first2 = (byte)((currentSphereTime.Year & 0b1100000000) >> 8);
+            var fourthDateByte = (byte)(0b00110100 + years_first2);
 
             return new[]
             {
-                firstDateByte, secondDateByte,
-                thirdDateByte, years_last8, fourthDateByte
+                firstDateByte, secondDateByte, thirdDateByte, years_last8, fourthDateByte
             };
         }
     }
